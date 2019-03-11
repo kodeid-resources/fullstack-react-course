8c7eac15c37c3dfc22481df0636fa895806665af
Riza Fahmi <rizafahmi@gmail.com>
2019-03-11 14:07:21 +0700

    Setup express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.info(`Server running at http://localhost:${PORT}!`);
+});
+
+module.exports = app;
