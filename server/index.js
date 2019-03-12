8eeb278b4c33801a81fdd18c30dc3ad518e94ba8
Riza Fahmi <rizafahmi@gmail.com>
2019-03-12 09:42:38 +0700

    Connect to weather API through model

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const getWeatherByCity = require('../models/weather.js');
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
