34ddfa88f5d48669714f3f3cd4ebb7c8c860fd05
Riza Fahmi <rizafahmi@gmail.com>
2019-03-12 13:50:47 +0700

    Finish weather route

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/cities', require('../api/cities.js'));
+app.use('/api/weather', require('../api/weather.js'));
 
 app.listen(PORT, () => {
   console.info(`Server running at http://localhost:${PORT}!`);
