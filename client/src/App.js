3618b16e1b2f67a2373494d5f0af78676470e90a
Riza Fahmi <rizafahmi@gmail.com>
2019-03-19 16:19:55 +0700

    Refactor to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import AddCity from './components/AddCity.js';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -51,21 +53,11 @@ class App extends Component {
       <div className="container text-center mx-auto my-20 text-grey-darkest">
         <h1 className="my-3 text-grey-darker">My Awesome Weather Dashboard</h1>
         <p>The current weather for your favorite cities!</p>
-        <form className="px-8 pt-6 pb-8 mb-4" onSubmit={this.handleSubmit}>
-          <input
-            placeholder="Add your favorite city"
-            type="text"
-            className="shadow border rounded w-3/5 py-2 px-3 text-grey-dark leading-tight"
-            onChange={this.handleInputChange}
-            value={this.state.newCity}
-          />
-          <button
-            type="submit"
-            className="w-1/5 bg-teal hover:bg-teal-dark text-white font-bold py-2 px-3 mx-2 rounded"
-          >
-            Add City
-          </button>
-        </form>
+        <AddCity
+          handleSubmit={this.handleSubmit}
+          handleInputChange={this.handleInputChange}
+          newCity={this.state.newCity}
+        />
       </div>
     );
   }
