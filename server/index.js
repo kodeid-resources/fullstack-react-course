ee531e316fa0332676d3aeb8d4791e1810566527
Riza Fahmi <rizafahmi@gmail.com>
2019-03-21 10:38:03 +0700

    Add static to client/build

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/cities', require('../api/cities.js'));
 app.use('/api/weather', require('../api/weather.js'));
 
+if (ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.info(`Server running at http://localhost:${PORT}!`);
 });
