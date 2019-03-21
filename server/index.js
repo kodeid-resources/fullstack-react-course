c32783a1d127ee17fd1b92ea7fe33c4dcae6ec06
Riza Fahmi <rizafahmi@gmail.com>
2019-03-21 11:21:34 +0700

    Change server for heroku

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use('/api/weather', require('../api/weather.js'));
 
 if (ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.use((req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
