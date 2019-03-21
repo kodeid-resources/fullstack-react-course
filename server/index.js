521cfeeb0562a63addab1eabf9a16c757a697ac9
Riza Fahmi <rizafahmi@gmail.com>
2019-03-21 11:24:32 +0700

    Another change

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ app.use('/api/weather', require('../api/weather.js'));
 
 if (ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+  // app.use((req, res) => {
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // });
 }
 
 app.listen(PORT, () => {
