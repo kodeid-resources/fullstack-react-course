af61101852b2dbcfc4e0474e81d2569bac849337
Riza Fahmi <rizafahmi@gmail.com>
2019-03-21 14:24:50 +0700

    Remove Class Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import AddCity from './components/AddCity.js';
 import Weather from './components/Weather.js';
